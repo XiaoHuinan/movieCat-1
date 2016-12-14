e6d4ebfe377637a29790c5398d61801d10ca5306
itcast <itcast@test.com>
2016-12-14 20:11:40 +0800

    05-在common文件夹中建立公共服务，用于发送jsonp请求

---
+++ app/common/JSONP.js
@@ -0,0 +1,28 @@
+(function(angular){
+  angular.module("moviecat.jsonp", [])
+    .service("JSONP", ["$window", function($window){
+      var doc = $window.document;
+
+      // jsonp方法的作用，用来发送jsonp请求获取数据
+      this.jsonp = function(url, params, callback){
+          url += "?";
+          for(var k in params){
+            url += k + "=" + params[k] + "&";
+          }
+
+          var cbName = "jsonp_" + (new Date() - 0);
+          url += "callback=" + cbName;
+
+          $window[cbName] = function(data){
+            callback(data);
+
+            document.body.removeChild(script);
+            delete window[cbName];
+          }
+
+          var script = document.createElement("script");
+          script.src = url;
+          doc.body.appendChild(script);
+      }
+    }]);
+})(angular)
--- app/in_theaters/module.js
+++ app/in_theaters/module.js
@@ -10,7 +10,7 @@
     // 4-1 注入$http，并请求数据
     .controller("InTheatersController", ["$scope", "$http", function($scope, $http){
       // 路径依然是以在哪执行的为参考
-      $http.get("./in_theaters/data.json")
+      /*$http.get("./in_theaters/data.json")
           // 数据请求成功后的回调函数
           .then(function(response){
             // console.log(response);
@@ -19,6 +19,16 @@
           // 数据请求失败后的回调函数
           }, function(response){
             // console.log(response);
+          })*/
+
+          // 由于豆瓣API中要求callback只能包含数字。字母下划线，且长度不大于50
+          // 但是在angular中所有的回调函数都是angular.callback._0 这种形式(带点)
+          // 这样与要求不符，无法使用angular中提供的jsonp请求
+          $http.jsonp("http://v.showji.com/Locating/showji.com2016234999234.aspx?m=13545158813&output=json&callback=JSON_CALLBACK&timestamp=' + (new Date()-0)")
+          .then(function(response){
+            console.log(response);
+          }, function(){
+            console.log("error");
           })
     }])
 })(angular)
